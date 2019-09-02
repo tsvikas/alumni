9148da08c08b2f39988af81ad1cdafa17f84db2e
Tsvika <tsvika@augmanitynano.com>
2019-09-02 15:36:35 +0300

    feat: save and load a basic estimator

---
+++ alumni/alumni.py
@@ -0,0 +1,160 @@
+import enum
+from pathlib import Path
+
+import tables
+
+PROTOCOL_NAME = "sklearn-hdf5"
+PROTOCOL_VERSION = "0.1"
+
+HDF_TITLE = "alumni, a python package to save complex scikit-learn estimators"
+ESTIMATOR_GROUP = "_estimator"
+FIT_GROUP = "_fit"
+
+
+class GroupType(enum.Enum):
+    ESTIMATOR = 1
+    FITTED_ATTRIBUTES = 2
+    LIST_OF_NAMED_ESTIMATORS = 3
+    # TODO: KERAS_REGRESSOR/CLASSIFIER/HISTORY
+
+
+def save_estimator(filename, estimator, fitted=True):
+    if Path(filename).exists():
+        raise ValueError(f"file {filename} exists")
+    with tables.File(str(filename), mode="w", title=HDF_TITLE) as hdf_file:
+        # save metadata
+        hdf_file.set_node_attr("/", "protocol_name", PROTOCOL_NAME)
+        hdf_file.set_node_attr("/", "protocol_version", PROTOCOL_VERSION)
+        # save estimator
+        group = hdf_file.create_group("/", ESTIMATOR_GROUP)
+        _save_estimator_to_group(hdf_file, group, estimator, fitted=fitted)
+        # TODO: save train / test / validation data
+
+
+def _save_estimator_to_group(hdf_file, group, estimator, fitted):
+    # save estimator metadata
+    class_name = estimator.__class__.__module__ + "." + estimator.__class__.__name__
+    module_version = getattr(__import__(estimator.__class__.__module__), "__version__")
+    hdf_file.set_node_attr(group, "__class_name__", class_name)
+    hdf_file.set_node_attr(group, "__module_version__", module_version)
+    hdf_file.set_node_attr(group, "__type__", GroupType.ESTIMATOR.name)
+
+    # save params
+    params_dict = get_params_dict(estimator)
+    _save_params_to_group(hdf_file, group, params_dict, fitted=False)  # TODO: check
+
+    if fitted:
+        # create fit group
+        fit_group = hdf_file.create_group(group, FIT_GROUP)
+        hdf_file.set_node_attr(fit_group, "__type__", GroupType.FITTED_ATTRIBUTES.name)
+        # save fit params
+        fit_params_dict = get_fit_params_dict(estimator)
+        _save_params_to_group(hdf_file, fit_group, fit_params_dict, fitted)
+
+
+def get_params_dict(estimator):
+    params_dict = estimator.get_params(deep=False)
+    return params_dict
+
+
+def get_fit_params_dict(estimator):
+    all_attrs = dir(estimator)
+    orig_attrs = dir(estimator.__class__)
+    fit_param_names = [p for p in all_attrs if p not in orig_attrs and p.endswith("_")]
+    assert not [p for p in fit_param_names if p.startswith("_")]
+    # TODO: add `and not p.startswith("_")` or remove `assert`
+    fit_params_dict = {
+        param_name: getattr(estimator, param_name) for param_name in fit_param_names
+    }
+    return fit_params_dict
+
+
+def _save_params_to_group(hdf_file, group, params_dict, fitted):
+    for param_name, param_value in params_dict.items():
+        if is_estimator(param_value):
+            param_group = hdf_file.create_group(group, param_name)
+            _save_estimator_to_group(hdf_file, param_group, param_value, fitted)
+        elif is_list_of_named_estimators(param_value):
+            param_group = hdf_file.create_group(group, param_name)
+            _save_list_of_named_estimators(hdf_file, param_group, param_value, fitted)
+        else:
+            hdf_file.set_node_attr(group, param_name, param_value)
+
+
+def is_estimator(param_value):
+    return hasattr(param_value, "_get_param_names")
+
+
+def is_list_of_named_estimators(param_value):
+    return (
+        isinstance(param_value, list)
+        and param_value
+        and isinstance(param_value[0], tuple)
+        and len(param_value[0]) >= 2
+        and is_estimator(param_value[0][1])
+    )
+
+
+def _save_list_of_named_estimators(hdf_file, group, estimator_list, fitted):
+    hdf_file.set_node_attr(group, "__type__", GroupType.LIST_OF_NAMED_ESTIMATORS.name)
+    hdf_file.set_node_attr(group, "names", [n for (n, e, *r) in estimator_list])
+    hdf_file.set_node_attr(group, "rests", [r for (n, e, *r) in estimator_list])
+    for (name, estimator, *_rest) in estimator_list:
+        sub_group = hdf_file.create_group(group, name)
+        _save_estimator_to_group(hdf_file, sub_group, estimator, fitted)
+
+
+def load_estimator(filename, fitted=True):
+    with tables.File(str(filename), mode="r") as hdf_file:
+        # check metadata
+        assert hdf_file.get_node_attr("/", "protocol_name") == PROTOCOL_NAME
+        assert hdf_file.get_node_attr("/", "protocol_version") == PROTOCOL_VERSION
+        # load estimator
+        group = hdf_file.get_node("/")[ESTIMATOR_GROUP]
+        estimator = _load_estimator_from_group(hdf_file, group, fitted=fitted)
+        return estimator
+
+
+def check_version(module_name, module_version):
+    assert module_version == getattr(__import__(module_name), "__version__")
+
+
+def _load_estimator_from_group(hdf_file, group, fitted):
+    attrs = group._v_attrs
+    user_attrs = {k: attrs[k] for k in attrs._f_list("user")}
+
+    group_type = GroupType[user_attrs.pop("__type__")]
+    if group_type == GroupType.ESTIMATOR:
+        module_name, class_name = user_attrs.pop("__class_name__").rsplit(".", 1)
+        module_version = user_attrs.pop("__module_version__")
+        check_version(module_name, module_version)
+
+        # TODO: add subgroups to user_attrs
+
+        mod = __import__(module_name, fromlist=[class_name])
+        klass = getattr(mod, class_name)
+        estimator = klass(**user_attrs)
+
+        if fitted:
+            fit_group = group[FIT_GROUP]
+            assert (
+                hdf_file.get_node_attr(fit_group, "__type__")
+                == GroupType.FITTED_ATTRIBUTES.name
+            )
+            fit_attrs = fit_group._v_attrs
+            fit_user_attrs = {k: fit_attrs[k] for k in fit_attrs._f_list("user")}
+            # TODO: add subgroups to fit_user_attrs
+
+            for k, v in fit_user_attrs.items():
+                setattr(estimator, k, v)
+
+        return estimator
+
+    elif group_type == GroupType.FITTED_ATTRIBUTES:
+        raise ValueError("_load_estimator_from_group got a group with fitting data")
+
+    elif group_type == GroupType.LIST_OF_NAMED_ESTIMATORS:
+        # TODO: code here
+        raise NotImplementedError("list of tuples")
+
+    raise NotImplementedError("unrecognized group type")
--- tests/test_alumni.py
+++ tests/test_alumni.py
@@ -1,5 +1,52 @@
-from alumni import __version__
+import numpy as np
+import tables
+from sklearn.preprocessing import OneHotEncoder
+
+from alumni import __version__, alumni
 
 
 def test_version():
     assert __version__ == "0.0.0"
+
+
+def test_save(tmp_path):
+    est = OneHotEncoder(handle_unknown="ignore")
+    X = [["Male", 1], ["Female", 3], ["Female", 2]]
+    est.fit(X)
+    fn = tmp_path / "est.hdf5"
+    alumni.save_estimator(fn, est, fitted=True)
+
+    with tables.open_file(str(fn), "r") as h:
+        root_attrs = h.root._v_attrs
+        assert root_attrs["protocol_name"] == alumni.PROTOCOL_NAME
+        assert root_attrs["protocol_version"] == alumni.PROTOCOL_VERSION
+
+        est_attrs = h.root[alumni.ESTIMATOR_GROUP]._v_attrs
+        assert (
+            est_attrs["__class_name__"]
+            == "sklearn.preprocessing._encoders.OneHotEncoder"
+        )
+        assert est_attrs["__type__"] == alumni.GroupType.ESTIMATOR.name
+        assert est_attrs["handle_unknown"] == "ignore"
+
+        fit_attrs = h.root[alumni.ESTIMATOR_GROUP][alumni.FIT_GROUP]._v_attrs
+        assert fit_attrs["__type__"] == alumni.GroupType.FITTED_ATTRIBUTES.name
+        categories_0, categories_1 = fit_attrs["categories_"]
+        assert (categories_0 == np.array(["Female", "Male"], dtype=object)).all()
+        assert (categories_1 == np.array([1, 2, 3], dtype=object)).all()
+
+        assert fit_attrs["drop_idx_"] is None
+
+
+def test_load(tmp_path):
+    est = OneHotEncoder(handle_unknown="ignore")
+    X = [["Male", 1], ["Female", 3], ["Female", 2]]
+    est.fit(X)
+    fn = tmp_path / "est.hdf5"
+    alumni.save_estimator(fn, est, fitted=True)
+    loaded_est = alumni.load_estimator(fn, fitted=True)
+    assert type(loaded_est) == type(est)
+    assert loaded_est.handle_unknown == est.handle_unknown
+    assert len(loaded_est.categories_) == len(est.categories_)
+    assert np.all(loaded_est.categories_[0] == est.categories_[0])
+    assert np.all(loaded_est.categories_[1] == est.categories_[1])
